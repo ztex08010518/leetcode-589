3aed07d084cea43fc96f6179baa33a7d2f12a196
40247004S <ztex080104518@gmail.com>
2018-11-16 12:37:02 +0800

    The first trial

--- leetcode-589/main.cpp
+++ leetcode-589/main.cpp
@@ -7,9 +7,29 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+    
+    Node() {}
+    
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
 
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        int i=0;
+    }
+};
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
